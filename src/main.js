59ea9fa8f27891747adf27a145543896a1669425
UnusualNick <59228271+UnusualNick@users.noreply.github.com>
2026-04-02 02:18:49 +0500

    feat: add points model and event types, enhance trip presenter and views

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 import TripPresenter from "./presenter/trip-presenter";
+import PointsModel from "./model/points-model";
 
 const filtersContainer = document.querySelector(".trip-controls__filters");
 const tripEventsContainer = document.querySelector(".trip-events");
+const pointsModel = new PointsModel();
 
 const tripPresenter = new TripPresenter({
   filtersContainer,
   tripEventsContainer,
+  pointsModel,
 });
 
 tripPresenter.init();
--- src/presenter/trip-presenter.js
+++ src/presenter/trip-presenter.js
@@ -4,37 +4,59 @@ import SortView from "../view/sort-view";
 import ListView from "../view/list-view";
 import PointView from "../view/point-view";
 import EditPointView from "../view/edit-point-view";
-import NewPointView from "../view/new-point-view";
-
-const POINTS_COUNT = 3;
 
 export default class TripPresenter {
   #filtersContainer = null;
   #tripEventsContainer = null;
+  #pointsModel = null;
 
-  constructor({ filtersContainer, tripEventsContainer }) {
+  constructor({ filtersContainer, tripEventsContainer, pointsModel }) {
     this.#filtersContainer = filtersContainer;
     this.#tripEventsContainer = tripEventsContainer;
+    this.#pointsModel = pointsModel;
   }
 
   init() {
+    const points = this.#pointsModel.points;
+    const destinations = this.#pointsModel.destinations;
+    const offers = this.#pointsModel.offers;
+    const destinationsById = new Map(
+      destinations.map((destination) => [destination.id, destination]),
+    );
+    const offersById = new Map(offers.map((offer) => [offer.id, offer]));
+
     const filtersComponent = new FiltersView();
     const sortComponent = new SortView();
     const listComponent = new ListView();
-    const editPointComponent = new EditPointView();
-    const newPointComponent = new NewPointView();
 
     render(filtersComponent, this.#filtersContainer);
     render(sortComponent, this.#tripEventsContainer);
     render(listComponent, this.#tripEventsContainer);
 
     const pointListContainer = listComponent.getElement();
+    const firstPoint = points[0] ?? null;
+
+    const editPointComponent = new EditPointView({
+      point: firstPoint,
+      destinations,
+      destinationsById,
+      offers,
+    });
+
+    const createPointComponent = new EditPointView({
+      destinations,
+      destinationsById,
+      offers,
+    });
 
     render(editPointComponent, pointListContainer, RenderPosition.AFTERBEGIN);
-    render(newPointComponent, pointListContainer);
+    render(createPointComponent, pointListContainer);
 
-    for (let i = 0; i < POINTS_COUNT; i++) {
-      render(new PointView(), pointListContainer);
-    }
+    points.forEach((point) =>
+      render(
+        new PointView({ point, destinationsById, offersById }),
+        pointListContainer,
+      ),
+    );
   }
 }
